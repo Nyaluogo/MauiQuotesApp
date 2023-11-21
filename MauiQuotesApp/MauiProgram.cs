cc0ff81cfa30f8eeab91bb1ed801074579a61650
Nyaluogo <edwinnyaluogo@gmail.com>
2023-11-21 07:15:29 +0300

    Add project files.

---
+++ MauiQuotesApp/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiQuotesApp;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("vcr_osd.ttf", "vcr_osd");
+			});
+
+		return builder.Build();
+	}
+}
